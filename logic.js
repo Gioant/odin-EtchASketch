a2b00b120b6ccdae51667ec5f3982eb3f7adf02c
Anthony <gioant1@hotmail.com>
2022-12-02 23:36:36 -0500

    replaced logic by putting it inside a function
    will use this when needed

--- logic.js
+++ logic.js
@@ -1,17 +1,24 @@
 //get container of grid
 const container = document.getElementById("sketchPad");
 
+
 //apply some style to it
 container.style.display = "grid";
-container.style.gridTemplateColumns = "repeat(16,1fr)";
-container.style.gridTemplateRows = "repeat(16,1fr)";
+container.style.gridTemplateColumns = "repeat(10,1fr)";
+container.style.gridTemplateRows = "repeat(10,1fr)";
 
 
-//for loop to generate square of div
-for(let i = 0; i < 256; i++){
-    const square = document.createElement("div");
-    square.className = "boxes";
-    square.style.backgroundColor = "white";
-    square.style.border = "1px solid black";
-    container.appendChild(square);
+
+function createGrid(value) {
+    let size = value * value;
+
+    for (let i = 1; i <= size; i++) {
+        const square = document.createElement("div");
+        square.className = "boxes";
+        square.style.backgroundColor = "white";
+        square.style.border = "1px solid black";
+        container.style.gridTemplateColumns = "repeat(value,1fr)";
+        container.style.gridTemplateRows = "repeat(value,1fr)";
+        container.appendChild(square);
+    }
 }
