5e75b87ba095fd0c30dbede459fdd91efbe88ba0
Anthony <gioant1@hotmail.com>
2022-12-03 16:22:03 -0500

    fixed issue with creating grid

--- logic.js
+++ logic.js
@@ -12,30 +12,27 @@ $('.ui.slider').slider({
     smooth: true,
     onChange: function (value) {
         p.innerHTML = value + "x" + value;
-        createGrid(value);
+        // createGrid(value);
     }
 });
 
 
-//apply some style to it
-container.style.display = "grid";
-container.style.gridTemplateColumns = "repeat(10,1fr)";
-container.style.gridTemplateRows = "repeat(10,1fr)";
-
 
 
 function createGrid(value) {
-    let size = value * value;
+    const size = value * value;
 
-    for (let i = 1; i <= size; i++) {
+    for (let i = 0; i < size; i++) {
         const square = document.createElement("div");
         square.className = "boxes";
         square.style.backgroundColor = "white";
         square.style.border = "1px solid black";
-        container.style.gridTemplateColumns = "repeat(value,1fr)";
-        container.style.gridTemplateRows = "repeat(value,1fr)";
+
+        square.style.width = "20px";
+        square.style.height = "20px";
+
+        container.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${value}, 1fr)`;
         container.appendChild(square);
     }
 }
-
-createGrid()
