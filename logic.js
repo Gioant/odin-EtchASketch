6f86b80328d23413851a8f08029109215c3f6000
Anthony <gioant1@hotmail.com>
2022-12-05 00:33:42 -0500

    did the following:
    -added detailed comments
    - fixed logic inside create grid function
    - added resize function that calls create grid function using value of input slider

--- logic.js
+++ logic.js
@@ -3,8 +3,10 @@ const grid = document.getElementById("canvas");
 const slider = document.getElementById("slider");
 const p = document.getElementById("value");
 
+startingGrid();
 
-//add event listener for slider
+
+//add event listener for slider on change
 slider.addEventListener('change', () => {
     const value = slider.value;
 
@@ -12,19 +14,34 @@ slider.addEventListener('change', () => {
     p.innerHTML = value + "x" + value
 
     //call function to ReSize grid
-
+    resizeGrid(value);
 });
 
 
-function createGrid() {
+//function to create grid
+function createGrid(value) {
+    const size = value * value;
 
-    for (let i = 0; i < 25; i++) {
+    //first style the container using product of value
+    grid.style.gridTemplateRows = "repeat(value,1fr)";
+    grid.style.gridTemplateColumns = "repeat(value,1fr)";
+
+    //loop until we reach grid size
+    for (let i = 0; i < size; i++) {
         const cell = document.createElement('div');
         cell.className = "boxes";
-        cell.style.gridTemplateColumns = "repeat(value,1fr)";
-        cell.style.gridTemplateRows = "repeat(value,1fr)";
         grid.appendChild(cell);
     }
 }
 
-createGrid();
+
+
+//function to resize grid based on value of slider
+function resizeGrid(value){
+    let boxes = grid.querySelectorAll("div");
+    //for each box, remove it
+    boxes.forEach(box => box.remove());
+
+    //call function to create grid with value
+    createGrid(value);
+}
