ab517e855e0d66399abad1fa29ccb64f8b62a9fe
Anthony <gioant1@hotmail.com>
2022-12-04 23:36:12 -0500

    added comments & created event listener for input slider

--- logic.js
+++ logic.js
@@ -1,7 +1,16 @@
-//get container of grid
+//get necessary elements from page
 const container = document.getElementById("sketchPad");
-const slider = document.getElementById("slider1");
-const p = document.getElementById("sizeLbl");
+const slider = document.getElementById("slider");
+const p = document.getElementById("value");
 
 
+//add event listener for slider
+slider.addEventListener('change', () => {
+    const value = slider.value;
 
+    //change p element to show correct value
+    p.innerHTML = value + "x" + value
+
+    //call function to ReSize grid
+
+});
